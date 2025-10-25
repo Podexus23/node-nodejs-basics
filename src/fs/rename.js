793cb06d835b36661dcfbf1a3e1d4530f4b1a532
Podexus23 <santon23@gmail.com>
2025-10-25 21:37:38 +0300

    feat: fs rename and copy tasks are ready

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-  // Write your code here
+  const oldName = resolve(__dirname, "files", "wrongFilename.txt");
+  const newName = resolve(__dirname, "files", "properFilename.md");
+
+  try {
+    const oldExists = await fs
+      .stat(oldName)
+      .then(() => true)
+      .catch(() => false);
+    const newExists = await fs
+      .stat(newName)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!oldExists || newExists) {
+      throw new Error("FS operation failed");
+    }
+
+    await fs.rename(oldName, newName);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await rename();
