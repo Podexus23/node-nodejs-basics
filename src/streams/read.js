25bf36354280c0676f0a9317ade69704ad27e185
Podexus23 <santon23@gmail.com>
2025-10-26 01:04:05 +0300

    feat: streams task done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { createReadStream } from "fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-  // Write your code here
+  const filePath = resolve(__dirname, "files", "fileToRead.txt");
+  const rs = createReadStream(filePath, { encoding: "utf-8" });
+
+  rs.pipe(process.stdout);
+
+  rs.on("end", () => {
+    process.stdout.write("\n");
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { createWriteStream } from "fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-  // Write your code here
+  const filePath = resolve(__dirname, "files", "fileToWrite.txt");
+  const rs = createWriteStream(filePath, { encoding: "utf-8" });
+  process.stdin.pipe(rs);
 };
 
 await write();
