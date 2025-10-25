f2329ae2fb40d7570e884b7a01718f3fa968c4a8
Podexus23 <santon23@gmail.com>
2025-10-25 22:51:34 +0300

    feat: fs delete and list task are done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-  // Write your code here
+  const fileToRemove = resolve(__dirname, "files", "fileToRemove.txt");
+
+  try {
+    await fs.rm(fileToRemove);
+  } catch (err) {
+    if (err.code === "ENOENT") throw new Error("FS operation failed");
+    throw err;
+  }
 };
 
 await remove();
