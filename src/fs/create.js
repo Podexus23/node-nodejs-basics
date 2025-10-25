ac4874b5107b33f12648689130283d7d2b55039c
Podexus23 <santon23@gmail.com>
2025-10-25 21:01:58 +0300

    feat: fs create task complete

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from "url";
+import { writeFile } from "fs/promises";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-  // Write your code here
+  const filePath = resolve(__dirname, "files", "fresh.txt");
+  const fileData = `I am fresh and young`;
+
+  try {
+    await writeFile(filePath, fileData, { flag: "wx" });
+  } catch (err) {
+    if (err.code === "EEXIST") throw new Error("FS operation failed");
+    throw err;
+  }
 };
 
 await create();
