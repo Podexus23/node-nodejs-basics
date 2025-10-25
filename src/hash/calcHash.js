a6b525fd4535baf794c243dbad2bfbe70af7a7c6
Podexus23 <santon23@gmail.com>
2025-10-26 00:25:36 +0300

    feat: hash task done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createReadStream } from "node:fs";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { pipeline } from "node:stream/promises";
+const { createHash } = await import("node:crypto");
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-  // Write your code here
+  const filePath = resolve(__dirname, "files", "fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+  const input = createReadStream(filePath);
+
+  await pipeline(input, hash);
+  const digest = hash.digest("hex");
+  // process.stdout.write(`${digest}\n`);
+  console.log(digest);
 };
 
 await calculateHash();
