a63147e65fac6d13edc5deb942ffd6084f8e36f0
Podexus23 <santon23@gmail.com>
2025-10-26 14:11:52 +0300

    feat: zlib tasks done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { createGzip } from "zlib";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-  // Write your code here
+  const input = resolve(__dirname, "files", "fileToCompress.txt");
+  const output = resolve(__dirname, "files", "archive.gz");
+  const rs = createReadStream(input);
+  const ws = createWriteStream(output);
+  const gzip = createGzip();
+
+  try {
+    await pipeline(rs, gzip, ws);
+    console.log("File compressed");
+  } catch (err) {
+    console.error(`Zip: pipeline failed`, err.code || err);
+  }
 };
 
 await compress();
